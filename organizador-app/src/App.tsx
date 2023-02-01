db4a712d85094fcdebd569503cd9d42ad7c37ca3
Angel Federico <121796085+Angelfederico@users.noreply.github.com>
2023-02-01 01:49:13 +0000

    ...

--- organizador-app/src/App.tsx
+++ organizador-app/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Aplicacion  de organizador de archivos</h1>
+    <>
+      <h1>Aplicacion  de organizador de archivos</h1>
+    </>
   )
 }
 
